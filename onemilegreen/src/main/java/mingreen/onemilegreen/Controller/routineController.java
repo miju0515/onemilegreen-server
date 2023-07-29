f1f5a6d464909f36ad101cc46263020f7f726a10
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-07-29 18:37:04 +0900

    [feat]: 루틴 API 구현 (#3)
    루틴 인증을 위한 사진 업로드와 해당 루틴을 인증한 사람들의 정보를 가져오는 API 구현

--- onemilegreen/src/main/java/mingreen/onemilegreen/Controller/routineController.java
+++ onemilegreen/src/main/java/mingreen/onemilegreen/Controller/routineController.java
@@ -4,27 +4,33 @@ package mingreen.onemilegreen.Controller;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import mingreen.onemilegreen.Domain.UserRoutine;
+import mingreen.onemilegreen.Service.RoutineService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-public class uploadController {
+@RequestMapping(path="/routine")
+public class routineController {
     @Value("${cloud.aws.s3.bucket}")
     private  String bucket;
 
     private final AmazonS3Client amazonS3Client;
+    private final RoutineService routineService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){
+    @PostMapping("/routineUploadFile")
+    public ResponseEntity<String> uploadFile(@RequestParam("file")MultipartFile file,int user_rou_id,String urd_content){
         try{
             String fileName = UUID.randomUUID()+"-"+file.getOriginalFilename();
 
@@ -35,11 +41,21 @@ public class uploadController {
             String fileUrl=amazonS3Client.getUrl(bucket,fileName).toString();
 
             amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
-            return ResponseEntity.ok(fileUrl);
+
+            routineService.userRoutineUpdate(user_rou_id);
+            routineService.insertRoutine(fileUrl,urd_content,user_rou_id);
+            routineService.addMileEffect(user_rou_id);
+
+            return ResponseEntity.ok("인증이 완료되었습니다");
         }catch(IOException e){
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @PostMapping("/findAllUserRoutine")
+    public List<UserRoutine> findAllUserRoutine(int rou_id){
+        return routineService.findAll(rou_id);
     }
 
 }
